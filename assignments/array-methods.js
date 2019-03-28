195d07adb5381055621a4af72546213232833378
Douglas Campbell <dacampbelltbone@gmail.com>
2019-03-28 01:51:21 -0400

    Completed MVP JS-II and 1 Stretch

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,56 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(runner => fullName.push(`${runner.first_name} ${runner.last_name}`));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map(runner => allCaps.push(`${runner.first_name.toUpperCase()}`));
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+runners.filter(runner => {
+    if(runner.shirt_size === "L") {
+        largeShirts.push(runner);
+    }
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+const total = runners.reduce((accValue, currValue) => accValue + currValue.donation, 0);
+ticketPriceTotal.push(total);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// The event director needs the names and donation amounts to post on the event website
+// return the full names and the donation back in an array and log it.
+
+let donations = [];
+runners.forEach(runner => donations.push(`${runner.first_name} ${runner.last_name}: ${runner.donation}`));
+console.log(donations);
 
 // Problem 2
+// Event director needs each runner's company names w/o duplicates for banner. get a list of companies and log result. 
+const companies = runners.reduce((accCo, currCo) => {
+    if(accCo.indexOf(currCo.company_name) === -1) {
+        accCo.push(currCo.company_name);
+    }
+    return accCo;
+}, []);
+console.log(companies);
 
 // Problem 3
+// A generous donor wants to add $50 to each runners' donation.  Please return a list of the updated donations with the donor's contribution and log it.
+
+let newDonations = [];
+runners.map(runner => newDonations.push(runner.donation + 50));
+console.log(newDonations);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function crapFunc() {
+  let stuff = 12;
+  function innerCrap() {
+    console.log(stuff);
+  }
+  return innerCrap();
+}
 
+crapFunc();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,10 +16,14 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  return () => count += 1;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
